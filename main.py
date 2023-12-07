ef02df89301fea5e9cb56335ad377b8ee679cbab
alexaguerria19 <aaguerria@icloud.com>
2023-12-07 13:45:36 -0800

    work day

--- main.py
+++ main.py
@@ -4,24 +4,32 @@
 https://youtu.be/nF_crEtmpBo?si=jz-qfuisnQHB_mM3
 '''
 
-from settings import *
-from sprites import *
+from grid import Grid
 
-import pygame
+import pygame, sys
 
 pygame.init()
+dark_blue = (44, 44, 127)
 
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Tetris")
 
 clock = pygame.time.Clock()
 
+game_grid = Grid()
+game_grid.print_grid()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
+    
+    #Drawing Colors
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
             
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(60)
 
 
